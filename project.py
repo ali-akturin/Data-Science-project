a10da541391af6f0661ce623cd4ffb7d8eb13d00
ali-akturin <60317192+ali-akturin@users.noreply.github.com>
2021-04-05 00:14:30 +0500

    Update project.py

--- project.py
+++ project.py
@@ -17,6 +17,12 @@ tickerSymbol = 'NFLX'
 tickerData = yf.Ticker(tickerSymbol)
 
 tickerDf = tickerData.history(period= '1d', start='2010-5-31', end='2020-5-31')
-
+st.write("""
+## Closing price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume
+
+""")
 st.line_chart(tickerDf.Volume)
